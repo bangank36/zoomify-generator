69a352a2a71dfba56f38ad59c033efed40db06bc
bangank36 <bangank36@gmail.com>
2023-03-19 19:33:39 +0700

    - Create output.json file

--- run.php
+++ run.php
@@ -8,13 +8,75 @@ use DanielKm\Zoomify\ZoomifyFactory;
 // Set your configuration options
 $globalConfig = [
     // Add your config options here
-    'titleType' => 'zoomify',
+    'tileFormat' => 'zoomify',
     'tileSize' => 256,
     'tileOverlap' => 0,
     'tileQuality' => 100,
     'destinationRemove' => true,
     'processor' => 'GD'
 ];
+
+function getFolderSizeAndFileCount($dir) {
+    $size = 0;
+    $count = 0;
+
+    foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS)) as $file) {
+        $size += $file->getSize();
+        $count++;
+    }
+
+    return ['size' => $size, 'count' => $count];
+}
+
+function resizeImage($source, $destination, $newWidth) {
+    // Get the original image dimensions
+    list($width, $height) = getimagesize($source);
+
+    // Calculate the new height based on the aspect ratio
+    $newHeight = intval(($newWidth / $width) * $height);
+
+    // Create a new image with the new dimensions
+    $newImage = imagecreatetruecolor($newWidth, $newHeight);
+
+    // Get the image extension
+    $imageExtension = strtolower(pathinfo($source, PATHINFO_EXTENSION));
+
+    // Load the source image based on the extension
+    switch ($imageExtension) {
+        case 'jpeg':
+        case 'jpg':
+            $image = imagecreatefromjpeg($source);
+            break;
+        case 'png':
+            $image = imagecreatefrompng($source);
+            break;
+        case 'gif':
+            $image = imagecreatefromgif($source);
+            break;
+        default:
+            throw new Exception('Unsupported image format');
+    }
+
+    // Resize the image
+    imagecopyresampled($newImage, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    // Save the resized image
+    imagejpeg($newImage, $destination, 100);
+
+    // Free memory
+    imagedestroy($newImage);
+    imagedestroy($image);
+
+    return array(
+        'fullWidth' => $newWidth,
+        'fullHeight' => $newHeight,
+        'originWidth' => $width,
+        'originHeight' => $height,
+        'fullImage' => "full.jpg",
+    );
+}
+
+$output = array();
 
 // Define an $images array containing the image configurations.
 $images = [
@@ -49,12 +111,46 @@ foreach ($images as $image) {
         $config = array_merge($globalConfig, $image['config']);
 
         $source = $image['source'];
-        $destination = $image['destination'].'--'.$config['titleType'];
+        $destination = $image['destination'].'--'.$config['tileFormat'];
 
         // Setup the Zoomify library with the merged config
         $zoomify = $factory($config);
 
         // Process the source file and save tiles in the destination folder
         $result = $zoomify->process($source, $destination);
+
+        // ... rest of the code
+        $folderInfo = getFolderSizeAndFileCount($destination);
+        $folderSize = $folderInfo['size'] / 1048576; // Convert to megabytes
+        $fileCount = $folderInfo['count'];
+
+        // Grab the result of all images into JSON file
+        $fileOutput = array(
+            "image" => $image["name"].'--'.$config['tileFormat'],
+            "status" => $result,
+            "fileCount" => $fileCount - 1,
+            "folderSize" => $folderSize,
+            "tileSize" => $config['tileSize'],
+            "tileOverlap" => $config['tileOverlap'],
+            "format" => $config['tileFormat']
+        );
+
+        // Create a resized version of the image
+        $resizedDestination = $destination . "/full.jpg";
+        $imageInfo = resizeImage($source, $resizedDestination, 2500);
+        
+        // Concat the imageInfo with output[]
+        $fileOutput = array_merge($fileOutput, $imageInfo);
+
+        $output[] = $fileOutput;
     }
 }
+
+// Convert the array to a JSON string with pretty formatting
+$json = json_encode($output, JSON_PRETTY_PRINT);
+
+// Define the path to the output JSON file
+$outputFile = 'output/output.json';
+
+// Write the JSON string to the output file
+file_put_contents($outputFile, $json);
